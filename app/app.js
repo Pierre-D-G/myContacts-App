73ed53f75566e21f4bf6f1e1e1f058d154331092
Pierre-D-G <Pierre-D-G@users.noreply.github.com>
2017-04-20 10:49:20 -0400

    More bower components and app setup

--- app/app.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('myContacts', [
-  'ngRoute'
-]).
-config(['$routeProvider', function($routeProvider) {
-
-  $routeProvider.otherwise({redirectTo: '/contacts'});
-}]);
