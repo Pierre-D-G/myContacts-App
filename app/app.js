e029329c30872beb2dffc79994573bcc08fc72c1
Pierre-D-G <Pierre-D-G@users.noreply.github.com>
2017-04-20 10:23:01 -0400

    Cleaning up angular seed

--- app/app.js
+++ app/app.js
@@ -1,14 +1,9 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  'ngRoute'
 ]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+config(['$routeProvider', function($routeProvider) {
 
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
